aef6745dc606ce77a49cc77a1dc9f81c51e983ef
Daniel Fone <daniel@fone.net.nz>
2014-01-13 20:23:34 +1300

    Better post layout

--- config.rb
+++ config.rb
@@ -45,17 +45,12 @@ page "/feed.xml", layout: false
 ###
 
 # Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", layout: false
-#
-# With alternative layout
+
 page "*", layout: :page
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+
+with_layout :post do
+  page "/blog/*.markdown"
+end
 
 # Proxy pages (http://middlemanapp.com/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
@@ -68,11 +63,11 @@ page "*", layout: :page
 activate :directory_indexes
 
 # Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+helpers do
+  def format_date(date)
+    date.strftime('%e %b, %Y')
+  end
+end
 
 set :css_dir, 'stylesheets'
 
