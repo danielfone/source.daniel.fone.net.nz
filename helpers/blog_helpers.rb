0fccc49b79a794fdca78b0e627b86d392340de6e
Daniel Fone <daniel@fone.net.nz>
2020-09-10 12:31:03 +1200

    Added reading time

--- helpers/blog_helpers.rb
+++ helpers/blog_helpers.rb
@@ -8,4 +8,13 @@ module BlogHelpers
     blog.articles.select { |a| a.data[:featured] }.sample(3)
   end
 
+  def reading_time(input)
+    words_per_minute = 200
+
+    words = input.split.size
+    minutes = (words/words_per_minute).floor
+    minutes_label = minutes === 1 ? ' minute' : ' minutes'
+    minutes > 0 ? "about #{minutes} #{minutes_label}" : 'less than 1 minute'
+  end
+
 end
